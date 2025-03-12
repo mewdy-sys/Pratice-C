ab83e48d0efcd7e5c9846f02fbf296ce942528d7
mewdy-sys <cred633@gmail.com>
2025-03-12 05:41:46 +0500

    1

---
+++ D1/L1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Rectange
+{
+    double side1, side2;
+
+    public Rectange(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    public double AreaCalculator()
+    {
+        return side1 * side2;
+    }
+
+    public double PerimeterCalculator()
+    {
+        return (side1 + side2) * 2;
+    }
+
+    public double Area
+    {
+        get { return AreaCalculator(); }
+    }
+
+    public double Perimeter
+    {
+        get { return PerimeterCalculator(); }
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите первую сторону прямоугольника:");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите вторую сторону прямоугольника:");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        Rectange rectange = new Rectange(side1, side2);
+
+        Console.WriteLine($"Площадь = {rectange.Area}");
+        Console.WriteLine($"Площадь = {rectange.Perimeter}");
+
+        Console.ReadKey();
+    }
+}
---
+++ D1/L2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Employee
+{
+    private string firstName;
+    private string lastName;
+    private string position;
+    private double salary;
+    private int experience;
+
+    public Employee(string lastName, string firstName)
+    {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public void PosAndExp(string position, int experience)
+    {
+        this.position = position;
+        this.experience = experience;
+    }
+
+    public double CalcSalary()
+    {
+        if (position == "Главный")
+        {
+            salary = 1000 + 500 * experience;
+        }
+        else if (position == "Не главный")
+        {
+            salary = 500 + 250 * experience;
+        }
+        return salary;
+    }
+
+    public double Nalog()
+    {
+        return salary * 0.13;
+    }
+
+    public void Information()
+    {
+        Console.WriteLine("О сотруднике:");
+        Console.WriteLine($"{lastName}, {firstName}");
+        Console.WriteLine($"Зп: {CalcSalary()}");
+        Console.WriteLine($"Опыт: {experience}, Должность: {position}");
+        Console.WriteLine($"Налог: {Nalog()}");
+    }
+ }
+
+class Program
+{
+    static public void Main()
+    {
+        Employee employee = new Employee("Собянин", "Максим");
+        employee.PosAndExp("Главный", 100);
+
+        employee.Information();
+
+        Console.ReadKey();
+    }
+}
